6a921c2750a7fb28c251bd00d58660722dcd3409
Rewa <rewashete@gmail.com>
2026-03-03 18:10:24 +0530

    connected to db

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import path from "path"
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import {connectDB} from "./config/connectMongoDB.js";
+
+dotenv.config(); 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded) basically use x-www-form-urlencoded in postman to send req to the server to test 
+
+app.use(cookieParser());
+
+// if (process.env.NODE_ENV ==	= "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
+
+app.get("/", (req,res) => {
+	res.send("Server is ready");
+})
+
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectDB();
+	// console.log(`MONGO_URI=`${process.env.MONGO_URI});
+});
