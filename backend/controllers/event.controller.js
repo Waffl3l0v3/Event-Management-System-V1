64886e5a180dfbf0162e5e392bde8b6b1bff3fe2
Soham Kadam <sohamkadam0311@gmail.com>
2026-03-04 20:30:34 +0530

    rewa fucked up

--- backend/controllers/event.controller.js
+++ backend/controllers/event.controller.js
@@ -7,14 +7,57 @@ export const validateLocation = async (location) => {
     const response = await axios.get(
     `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${process.env.GEOCODE_KEY}`
     );
-};
 
-export const createEvent = async (req, res) => {
-    const { title, description, date, location, capacity, coverimg="" } = req.body;
-    const token = req.cookies.jwt;
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.userId).select("-password");  
-    return res.status(200).json({msg:"registered for the event"});
-    
+    if (response.data.results.length === 0) {
+        return false;
+    }
+
+    return true;
+    };
+export const create = async (req, res) => {
+    try {
+        const { title, description, date, location, capacity, coverimg="" } = req.body;
+        const user = req.user;
+
+        const isValid = await validateLocation(req.body.location);
+
+        if (!isValid) {
+            return res.status(400).json({error: "Invalid event location"});
+        }
+
+        if (description.length) { 
+            return res.status(400).json({ error: "Description length should be minimum 30 characters" });
+        }
+
+        const eventDate = new Date(date);
+        const today = new Date();
+        today.setHours(0,0,0,0);
+        eventDate.setHours(0, 0, 0, 0);
+        
+        if (eventDate < today) { 
+            return res.status(400).json({ error: "Event date cannot be in the past" });
+        }
+
+        if (!capacity) capacity = "none";
+        
+
+        const newEvent = Event({
+            title,
+            description,
+            date,
+            location,
+            capacity,
+            status: "upcoming",
+            organiser: user.username,
+            coverimg: coverimg
+        });
+        res.status(200).json({
+            "msg": "event registered",
+            "decoded": decoded,
+        })
+    }
+    catch (error) { }
+
+
 
 };
